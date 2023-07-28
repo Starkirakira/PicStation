dc322fde63c4e660cb42cf6868f9d5e5ff182f64
nya <1179284845@qq.com>
2023-07-28 23:57:42 +0800

    once

--- src/index.js
+++ src/index.js
@@ -1,1 +1,8 @@
 import Masonry from "masonry-layout";
+import imagesLoaded from 'imagesloaded';
+
+function importAll(r) { 
+    return r.keys().map(r);
+}
+
+const images = importAll(require, context('./public/pic', false, /\.(jpe?g|png|gif)$/));
