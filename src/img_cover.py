4fce71d47654afb6958a571764facb3c47777e39
Nya <1179284845@qq.com>
2023-08-10 17:38:51 +0800

    Draw text

--- src/img_cover.py
+++ src/img_cover.py
@@ -1,19 +1,56 @@
+import fractions
 from PIL import Image, ImageDraw, ImageFont, ImageOps
+import exifread
 
 
 def process_imnage(file_path, output_path):
-    image = Image.open(file_path)
+    with open(file_path, 'rb') as file:
+        image = Image.open(file_path)
+        file.seek(0)
+        tags = exifread.process_file(file, details=False)
+        # 设备制造商和型号
+        model = tags.get('Image Model')
 
+        # 曝光时间
+        exposure_time = tags.get('EXIF ExposureTime')
+
+        # 光圈大小（f-number）
+        aperture_tag = tags.get('EXIF FNumber')
+        aperture = float(fractions.Fraction(aperture_tag.values[0].num,
+                                            aperture_tag.values[0].den))
+
+        # ISO速度
+        iso_speed = tags.get('EXIF ISOSpeedRatings')
+
+        # 镜头信息
+        lens = tags.get('EXIF LensModel')
+
+        print(f'lens: {lens}')
+        print(f'Model: {model}')
+        print(f'Exposure Time: {exposure_time}')
+        print(f'Aperture: {aperture}')
+        print(f'ISO Speed: {iso_speed}')
     # difine image's border
-    border = 100
-    image_with_border = ImageOps.expand(image, border=border, fill="white")
+    border = (0, 0, 0, 200)
+    bg_color = (225, 225, 225)
+    image_with_border = ImageOps.expand(image, border=border, fill=bg_color)
     draw = ImageDraw.Draw(image_with_border)
+    text_model = str(model)
+    text_lens = str(lens)
+    text_aperture = str(aperture)
+    text_iso = str(iso_speed)
+    text_exposure_time = str(exposure_time)
+
     # Pick font && size
     # font_size = 16
-    font = ImageFont.load_default()
-    text = "Nikon"
+    font = ImageFont.truetype('./public/font/AxureHandwriting.ttf', size=40)
+
     text_position = (border/2, image.height + border/2)
-    draw.text(text_position, text, font=font, fill="black")
+    draw.text(text_position, text_model, font=font, fill="black")
+    draw.text(text_position, text_lens, font=font, fill="black")
+    draw.text(text_position, text_aperture, font=font, fill="black")
+    draw.text(text_position, text_iso, font=font, fill="black")
+    draw.text(text_position, text_exposure_time, font=font, fill="black")
     image_with_border.save(output_path)
 
 
@@ -31,4 +68,4 @@ def process_imnage(file_path, output_path):
 #         print(f"Processed {filename}")
 
 
-process_imnage("./public/pic/DSC_0091.jpg", "./public/pic/DSC_0091_change.jpg")
+process_imnage("./public/pic/DSC_3294.jpg", "./public/pic/DSC_0091_change.jpg")
